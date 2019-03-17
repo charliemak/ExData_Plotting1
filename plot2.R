6f907a3a9060d16af6ea09b1037340f611f47de1
Charlie Makowski <cmakows5@HFHS.ORG>
2019-03-17 15:47:43 -0400

    updated R files to include some narrative description

--- plot2.R
+++ plot2.R
@@ -1,9 +1,11 @@
+##read unzipped data file (specified multiple arguments to increase download speed)
 data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE
                    , col.names = c("MeterDate", "MeterTime", "GlobalActivePwr"
                                    , "GlobalReactivePwr", "Voltage", "GlobalIntensity"
                                    , "Submetering1", "Submetering2", "Submetering3")
                    , nrows = 2100000, colClasses = c(rep("character",8), "numeric")
                    , comment.char = "", stringsAsFactors = FALSE
+##subsetting using which() to include only rows from Feb 1-2, 2007
         )[which
           (read.table("household_power_consumption.txt", sep = ";", header = TRUE
                       , col.names = c("MeterDate", "MeterTime", "GlobalActivePwr"
@@ -13,9 +15,12 @@ data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE
                       , comment.char = "", stringsAsFactors = FALSE)[, 1] 
             %in% c("1/2/2007","2/2/2007")), ]
 
+##concatenate date and time columns for final date/time column, drop the separate date
+##and time columns, and convert non-POSIXct character data to numeric
 data <- mutate(data, "MeterDttm" = lubridate::dmy_hms(paste(MeterDate, MeterTime)))[,c(10,3:9)]
 data <- data.frame(data[1], sapply(data[, 2:8], function(x) as.numeric(x[], na.rm=TRUE)))
 
+##set png graphics device with file name, create the plot, and reset graphics device
 png(filename = "plot2.png")
 plot(data$MeterDttm, data$GlobalActivePwr, xlab = "", ylab = "Global Active Power (kilowatts)", type = "l")
 dev.off()
